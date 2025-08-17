7e9f404477ed6954deec8ec9228e8ef6d9eb4624
Ramvignesh <ramvigneshnanban@gmail.com>
2025-08-17 16:09:01 +0530

    name is used

--- hello.js
+++ hello.js
@@ -3,9 +3,10 @@
 function greet(name) {
   console.warn("This is a warning message ✅");
   console.error("This is an error message ✅");
-
+  
   // ❌ Uncommenting this will break the pipeline
   // console.log("Debug log ❌");
+  return name;
 }
 
 greet("GitHub Actions");
